3adbb2f3fbfb38abdb2477974c71e991ec2dc381
Krishan Gupta <bkgupta@gmail.com>
2013-10-13 14:39:39 +0000

    got file uploads working

--- data/urls.py
+++ data/urls.py
@@ -8,12 +8,12 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
 	url(r'^$', views.DataFrameListView.as_view(), name="list"),
-	url(r'^import$', views.DataFrameCreateView.as_view(), name="import"),
-	#url(r'^import$', "data.views.list", name="import"),
-	#url(r'^import$', views.DataFrameCreate.as_view(), name="import"),
-		
+	url(r'^create$', views.DataFrameCreateView.as_view(), name="create"),
+	
 	url(r'^(?P<slug>[\w-]+)/$', views.DataFrameDetailView.as_view(), name="frame"),
+	url(r'^(?P<slug>[\w-]+)/importdata$', views.DataFrameDeleteView.as_view(), name="importdata"),
 	url(r'^(?P<slug>[\w-]+)/delete$', views.DataFrameDeleteView.as_view(), name="delete"),
+	#url(r'^(?P<slug>[\w-]+)/delete$', views.DataFrameDeleteView.as_view(), name="delete"),
 
 	#Need to get this working for columns
 	#url(r'^(?P<slug>[\w-]+)/$', views.DataFrameDetailView.as_view(), name="frame"),
