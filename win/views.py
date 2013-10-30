9fee20213f92e2b38780d57eb08cd5c15ecee868
Krishan Gupta <bkgupta@gmail.com>
2013-10-30 14:52:38 +0000

    adding packages

--- win/views.py
+++ win/views.py
@@ -1,7 +1,4 @@
 from django.http import HttpResponse
-
-#def home(request):
-#    return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
     
 from django.views.generic import TemplateView
 
