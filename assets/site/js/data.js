9a862bc58a496bfc78c82bcc08d01446dd18fd3b
Krishan Gupta <bkgupta@gmail.com>
2013-11-09 19:19:15 +0000

    fixing static

--- assets/site/js/data.js
+++ assets/site/js/data.js
@@ -2,24 +2,18 @@
 // 	document.getElementById("uploadFile").value = this.value;
 // };
 
-function toTitleCase(str) {
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-$(document).ready(function(){
-	console.log("Document Loaded");
-
-	$("#report-rows").on( "sortreceive", function( event, ui ) {
-		elementName = event.toElement.id;
-		console.debug(elementName);
-	});
-});
-
 
 $(function() {
-	$(".connectedSortable").sortable({
+	$("#sortable1, #sortable2").sortable({
 		connectWith: ".connectedSortable"
 	}).disableSelection();
 });
 
+function toTitleCase(str) {
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+$(function() {
+	$( "#draggable" ).draggable();
+});
 
