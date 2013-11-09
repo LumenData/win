9e0f197c05b6663f4ce3741d650ae08bb67b3f22
Krishan Gupta <bkgupta@gmail.com>
2013-11-09 23:14:59 +0000

    reverting all changes related to data-manager name change of data app

--- assets/site/js/data.js
+++ assets/site/js/data.js
@@ -2,18 +2,24 @@
 // 	document.getElementById("uploadFile").value = this.value;
 // };
 
-
-$(function() {
-	$("#sortable1, #sortable2").sortable({
-		connectWith: ".connectedSortable"
-	}).disableSelection();
-});
-
 function toTitleCase(str) {
 	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-$(function() {
-	$( "#draggable" ).draggable();
+$(document).ready(function(){
+	console.log("Document Loaded");
+
+	$("#report-rows").on( "sortreceive", function( event, ui ) {
+		elementName = event.toElement.id;
+		console.debug(elementName);
+	});
 });
+
+
+$(function() {
+	$(".connectedSortable").sortable({
+		connectWith: ".connectedSortable"
+	}).disableSelection();
+});
+
 
--- win/settings/base.py
+++ win/settings/base.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     
+    'django_extensions',
     'south',
     'data',
     #'debug_toolbar',
